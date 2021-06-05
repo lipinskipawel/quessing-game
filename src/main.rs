eef8e72e6850387d1b1305dfe35c1bb45ebaec08
Pawel Lipinski <lipinskijob@gmail.com>
2021-06-05 11:38:37 +0200

    Add comapre functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -14,6 +15,13 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line.");
+    let guess: i32 = guess.trim().parse().expect("Please type a number!");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
